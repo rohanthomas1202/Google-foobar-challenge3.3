84ff9ff1170eb16402a97e58e8cad4495ca91f4e
rohanthomas1202 <rst170000@utdallas.edu>
2020-03-20 02:23:17 -0500

    a brute force approach to solving the problem

--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,32 @@
 public class Solution {
     public static void main(String[] args) {
+        System.out.println(solution(2,1));
+        System.out.println(solution(7,4));
+        System.out.println(solution(11,11));
+    }
 
+    /*
+    basic solution to the problem
+     */
+    public static String solution(int a, int b){
+
+        int first = a;
+        int second = b;
+        int turn = 0;
+        boolean check = false;
+
+        while(!(first == 1 && second == 1)){
+            if (first > second){
+                first = first - second;
+            }else if (second > first){
+                second = second - first;
+            }
+            else{
+                return "Impossible";
+            }
+
+            turn ++;
+        }
+        return String.valueOf(turn);
     }
 }
