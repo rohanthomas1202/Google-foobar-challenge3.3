109ab9c122a7ba9f502238c99d300608f45fe3b5
rohanthomas1202 <rst170000@utdallas.edu>
2020-03-20 14:22:39 -0500

    function solution takes in two strings not ints

--- src/Solution.java
+++ src/Solution.java
@@ -1,19 +1,18 @@
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(solution(2,1));
-        System.out.println(solution(7,4));
-        System.out.println(solution(11,11));
+        System.out.println(solution("2","1"));
+        System.out.println(solution("7","4"));
+        System.out.println(solution("11","11"));
     }
 
     /*
     basic solution to the problem
      */
-    public static String solution(int a, int b){
+    public static String solution(String a, String b){
 
-        int first = a;
-        int second = b;
+        int first = Integer.parseInt(a);
+        int second = Integer.parseInt(b);
         int turn = 0;
-        boolean check = false;
 
         while(!(first == 1 && second == 1)){
             if (first > second){
